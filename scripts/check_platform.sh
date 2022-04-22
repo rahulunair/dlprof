f5f2bbea68f5e68e4d2dcf478feffb69fef0d5f2
Rahul Nair <rahulunair@users.noreply.github.com>
2022-04-22 15:22:16 -0700

    Update check_platform.sh

--- scripts/check_platform.sh
+++ scripts/check_platform.sh
@@ -10,14 +10,47 @@ run()
   printf "%s\n" "$@"
   printf "%$((line_length+35))s\n" |tr " " "="
 }
-reqd_xtns=(avx512cd avx512bw avx512dq avx512f avx512vl)
+
 cpuxtns=$(lscpu | grep -i "avx512")
+
+run "[init]: checking if AVX512(FP32) instructions are available"
+reqd_xtns=(avx512cd avx512bw avx512dq avx512f avx512vl)
+
 for i in "${reqd_xtns[@]}"
   do 
     if [[ ! $cpuxtns =~ $i ]]
       then 
         run "[Error] : Intel® AVX-512 extensions are not available :: ($i)"
         exit
+      else
+        run "[Done]: Success, the platform supports AVX-512 instructions"
+    fi
+done
+
+run "[init]: checking if AVX-512 VNNI( int8) instructions are available"
+reqd_xtns=(avx512cd avx512bw avx512dq avx512f avx512vl)
+
+for i in "${reqd_xtns[@]}"
+  do 
+    if [[ ! $cpuxtns =~ $i ]]
+      then 
+        run "[Error] : Intel® AVX-512 VNNI( int8) extensions are not available :: ($i)"
+        exit
+      else
+        run "[Done]: Success, the platform supports AVX-512 VNNI( int8) instructions"
     fi
   done
-run "[Done]: Success, the platform supports AVX-512 instructions"
+
+run "[init]: checking if AMX(bf16, int8) instructions are available"
+reqd_xtns=(amx_tile)
+
+for i in "${reqd_xtns[@]}"
+  do 
+    if [[ ! $cpuxtns =~ $i ]]
+      then 
+        run "[Error] : Intel® AMX(bf16, int8) extensions are not available :: ($i)"
+        exit
+      else
+        run "[Done]: Success, the platform supports AMX(bf16, int8) instructions"
+    fi
+  done
